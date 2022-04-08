1606e8a938eb4f52bdec313ddc9d2665ae96bc3e
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2022-04-08 14:07:29 +0300

    ts config, run commands, compiler

---
+++ build/server/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function initServer() {
+    console.log("Server have been started!!");
+    console.log("Server!!df");
+}
+module.exports = {
+    initServer: initServer
+};
---
+++ src/server/index.ts
@@ -0,0 +1,8 @@
+function initServer() {
+    console.log("Server have been started!!");
+    console.log("Server");
+}
+
+module.exports = {
+    initServer: initServer
+}
