214ae2a93c67a44d452e05a14c0157e16d6d7aae
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2022-06-20 01:16:19 +0300

    search

--- src/server/models/user-model.ts
+++ src/server/models/user-model.ts
@@ -2,11 +2,10 @@
 const {Schema, model} = require("mongoose");
 
 const UserSchema = new Schema({
+    email: {type: String, unique: true, required: true, trim: true},
     firstName: {type: String, required: true},
     secondName: {type: String, required: true},
-    email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
-    confirmPassword: {type: String, required: true},
     isActivated: {type: Boolean, default: false},
     activationLink: {type: String},
 });
